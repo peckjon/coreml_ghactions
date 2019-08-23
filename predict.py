25012b57965a46ca553c5f46f69aae10361b1c42
Jon Peck <peckjon@gmail.com>
2019-08-23 12:56:06 -0700

    Simple house pricing estimator

---
+++ predict.py
@@ -0,0 +1,18 @@
+from coremltools.models import MLModel
+import csv
+
+
+def main():
+    model = MLModel('pricing.mlmodel') #thanks https://github.com/princeSmall/CoreMLModel
+    with open('input.csv') as input:
+        csv_reader = csv.reader(input, delimiter=',')
+        with open('output.csv', mode='w') as output:
+            csv_writer = csv.writer(output, delimiter=',')
+            for row in csv_reader:
+                sqft = row[0]
+                price = model.predict({'sqft': int(row[0])})['price']
+                csv_writer.writerow([sqft, int(price)])
+
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,15 @@
+from coremltools.converters.sklearn import convert
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# train simple linear regression model on prices.csv and export to CoreML
+def main():
+    data = pd.read_csv('prices.csv')
+    model = LinearRegression()
+    model.fit(data[["sqft"]], data["price"])
+    coreml_model = convert(model, "sqft", "price")
+    coreml_model.save('pricing.mlmodel')
+
+
+if __name__ == "__main__":
+    main()
