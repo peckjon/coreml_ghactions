6e346032493d4b0b906ffc012440fb82692d59b2
Jon Peck <peckjon@gmail.com>
2020-02-06 18:45:57 -0800

    Clean up output

--- predict.py
+++ predict.py
@@ -8,10 +8,10 @@ def main():
         with open('output.csv', mode='w') as output:
             csv_writer = csv.writer(output, delimiter=',')
             for row in csv_reader:
-                sqft = row[0]
-                price = model.predict({'sqft': int(row[0])})['price']
-                print([sqft, int(price)])
-                csv_writer.writerow([sqft, int(price)])
+                sqft = int(row[0])
+                price = int(model.predict({'sqft': sqft})['price'])
+                csv_writer.writerow([sqft, price])
+    print('Saved output.csv')
 
 
 if __name__ == "__main__":
