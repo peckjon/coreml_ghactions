7e7693067aef345811123c52a29a7f092a72bb6a
Jon Peck <peckjon@gmail.com>
2020-02-11 14:58:50 -0800

    narrow Pandas import

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 from coremltools.converters.sklearn import convert
-import pandas as pd
+from pandas import read_csv
 from sklearn.linear_model import LinearRegression
 
 # train simple linear regression model on prices.csv and export to CoreML
 def main():
-    data = pd.read_csv('prices.csv')
+    data = read_csv('prices.csv')
     model = LinearRegression()
     model.fit(data[["sqft"]], data["price"])
     coreml_model = convert(model, "sqft", "price")
